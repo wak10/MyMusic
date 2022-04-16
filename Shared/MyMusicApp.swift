8bce1fa5244924060fa27a2ce2c9c082a5b4ea76
wak10 <chiba.r25.taka@gmail.com>
2022-04-16 14:01:12 +0900

    Initial Commit

---
+++ Shared/MyMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMusicApp.swift
+//  Shared
+//
+//  Created by Takahiro Chiba on 2022/04/16.
+//
+
+import SwiftUI
+
+@main
+struct MyMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
